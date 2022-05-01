05bed284183cada3356d9185fddeabed4d4f23bf
Pravin <pravinmishra163@gmail.com>
2022-05-01 10:35:34 +0530

    users & app

--- Shopping-App/Backend/app.js
+++ Shopping-App/Backend/app.js
@@ -1,118 +1,49 @@
-<<<<<<< HEAD
-// import all the functions
-let express = require("express");
+//Load all required modules
+let express = require('express')
 let app = express();
-let cors = require("cors");
-let bodyParser = require("body-parser");
-let mongoClient = require("mongodb").MongoClient;
-let PORT = 3001;
-let dbURL = "mongodb://localhost:27017";
-// start the server 
-app.listen(PORT, () => console.log(`Server is running in ${PORT}`));
-
-// apply the middleware
-app.use(cors());
-app.use(bodyParser.json());
-
-//create the services for contact app
-// storing the users 
-app.post("/users", (request, response) => {
-    mongoClient.connect(dbURL, {useNewUrlParser:true}, (error, client) => {
-        if(error) {
-            throw error;
-        } else {
-            let db = client.db("grocery");
-            let users = request.body; // _id, name, phone, dob, password, contacts:[];
-            db.collection("users").insertOne(users, (err, res) => {
-                if(err) {
-                    response.status(409).json({"message":`users ${users._id} exists`})
-=======
-let express = require('express');
-let cors = require('cors');
 let bodyParser = require('body-parser');
-let mongoClient = require('mongodb').MongoClient;
+let mongoose = require('mongoose');
+let cors = require('cors');
 
-let PORT = 3001;
-let dbUrl = 'mongodb://localhost:27017';
+let url = 'mongodb://localhost:27017/myapp';
 
-let app = express();
-app.listen(PORT, () => console.log(`Server is Running at ${PORT}`));
+app.use(bodyParser.urlencoded({ extended: true })); // enable body part data
+app.use(bodyParser.json()); // json data.
+app.use(cors()); // used model from line 5
 
-app.use(cors());
-app.use(bodyParser.json());
+//Database connection without warning
+const mongooseDbOption = {
+  // to avoid warning
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex:true,
+  useFindAndModify:false
+};
+mongoose.connect(url, mongooseDbOption); //ready to connect
 
-app.post("/product", (request, response) => {
-    mongoClient.connect(dbUrl, {useNewUrlParser: true}, (error, client) => {
-        if (error) {
-            throw error;
-        } else {
-            let db = client.db('Grocerydb');
-            let product = request.body;
-            db.collection('products').insertOne(product, (err, res) => {
-                if (err) {
-                    response.status(400).json({'message': `${err}`});
->>>>>>> master
-                } else {
-                    response.status(201).json(res);
-                    client.close();
-                }
-            });
-        }
-    });
-});
-<<<<<<< HEAD
-=======
+mongoose.connection;
 
-app.delete('/product/:id', (request, response) => {
-    let id = parseInt(request.params.id);
-    mongoClient.connect(dbUrl, {useNewUrlParser: true}, (error, client) => {
-        if(error) {
-            throw error;
-        } else {
-            let db = client.db('Grocerydb');
-            db.collection('products').deleteOne({_id: id}).then((doc) => {
-                if(doc != null) {
-                    response.status(200).json(doc);
-                } else {
-                    response.status(404).json({"message": 'Sorry Id is Wrong'});
-                }
-                client.close();
-            });
-        }
-    });
-}); 
+//link to router module like a import concept.
 
-app.put('/product/:id/price/:num', (request, response) => {
-    let id = parseInt(request.params.id);
-    let price = parseInt(request.params.num);
-    mongoClient.connect(dbUrl, {useNewUrlParser: true}, (error, client) => {
-        if(error) {
-            throw error;
-        }else {
-            let db = client.db('Grocerydb');
-            db.collection('products').updateOne({_id: id}, {$set:{price: price} })
-                .then((doc) => {
-                    response.json(doc);
-                    client.close();
-                });
-        }
-    });
-});
+var Users = require('./Users/router/user.router.js');
+var Product = require('./Products/router/product.router.js');
+var Employee = require('./Employees/router/emp.router.js');
+var Admin = require('./Admin/router/admin.router.js');
+var Request = require('./Requests/router/requests.router.js');
+var Order = require('./Orders/router/orders.router.js');
+var Sales = require("./Sales/router/sales.router.js");
 
-app.put('/product/:id/quantity/:quantity',(request, response) => {
-    let id = parseInt(request.params.id);
-    let quantity = request.params.quantity;
-    mongoClient.connect(dbUrl, {useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }else {
-            let db = client.db('Grocerydb');
-            db.collection('products').updateOne({_id: id}, {$set: {quantity: quantity} })
-                .then((doc) => {
-                    response.json(doc);
-                    client.close();
-                })
-        }
-    })
-});
->>>>>>> master
+// http://localhost:9090/product/viewProducts   Get App Product Details
+// http://localhost:9090/product/selectProduct/102   Get App Product Details by Id
+// http://localhost:9090/product/storeProductDetails    rest client or post man {"pid":103,"pname":"Computer","price":43000}
+// http://localhost:9090/product/deleteProductById/101
+// http://localhost:9090/product/updateProductById  update
+//Middleware
+app.use('/product', Product);
+app.use('/', Users);
+app.use('/emp', Employee);
+app.use('/admin', Admin);
+app.use('/request', Request);
+app.use('/order', Order);
+app.use('/sales', Sales);
+app.listen(9090, () => console.log('Server is running on port number 9090'));
